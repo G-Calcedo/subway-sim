5fbba3f3b4f140c188b733f483b2484ec149166f
Pablo Fernández-Vega Padilla <45218480+Wonick97@users.noreply.github.com>
2021-06-29 20:07:21 +0200

    Muchas cosas (puntos trenes, trenes cambiados, chavalitos abarrotados en la linea) - Pablo

--- Assets/Scripts/UnitManagement/RandomTargetSelector.cs
+++ Assets/Scripts/UnitManagement/RandomTargetSelector.cs
@@ -8,6 +8,8 @@ public class RandomTargetSelector : MonoBehaviour
 
     public UnitSpawner trigger;
 
+    public List<TransformList> targetsTEST;
+
     private void Start()
     {
         trigger.OnUnitSpawned += SetRandomTarget;
@@ -15,6 +17,20 @@ public class RandomTargetSelector : MonoBehaviour
 
     private void SetRandomTarget(GameObject unit)
     {
-        unit.GetComponent<BasicMovement>().SetDestination(targets[Random.Range(0, targets.Count)].position);
+        //unit.GetComponent<BasicMovement>().SetDestination(targets[Random.Range(0, targets.Count)].position);
+
+        int randomValue = Random.Range(0, targetsTEST.Count);
+        //Debug.Log(randomValue + " "+ targetsTEST.Count);
+        TransformList targetRandom = targetsTEST[randomValue];
+
+        unit.GetComponent<BasicMovement>().SetDestination(new Vector3(Random.Range(targetRandom.list[1].position.x, targetRandom.list[0].position.x), targetRandom.list[0].position.y, 
+            Random.Range(targetRandom.list[2].position.z, targetRandom.list[0].position.z)));
+
     }
+}
+
+[System.Serializable]
+public class TransformList
+{
+    public List<Transform> list;
 }
