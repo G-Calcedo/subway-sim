f5b8b3ffe13bac468c80c928c60a045f02b0ec08
AVeraLopez <72403870+AVeraLopez@users.noreply.github.com>
2021-07-04 20:05:10 +0200

    Smaller camera changes

--- Assets/Scripts/Environment/CameraPivot.cs
+++ Assets/Scripts/Environment/CameraPivot.cs
@@ -7,9 +7,9 @@ public class CameraPivot : MonoBehaviour
 {
     private bool locked;
     private Camera camera;
-    private Vector3[] angles = new Vector3[4];
-    private Vector3[] positions = new Vector3[4];
-    private float[] FOV = new float[4];
+    private Vector3[] angles = new Vector3[5];
+    private Vector3[] positions = new Vector3[5];
+    private float[] FOV = new float[5];
     private int cont;
 
     private void Awake()
@@ -21,16 +21,19 @@ public class CameraPivot : MonoBehaviour
         positions[1] = new Vector3(-3.3f, -12.3f, -75f);
         positions[2] = new Vector3(-9f, 0.7f, 2.2f);
         positions[3] = new Vector3(53f, -15.7f, 1.25f);
+        positions[4] = new Vector3(-53.5f, -36f, 49f);
 
         angles[0] = new Vector3(0, 0, 0);
         angles[1] = new Vector3(-18f, -27f, -19f);
         angles[2] = new Vector3(-16f, -13.2f, -15.65f);
         angles[3] = new Vector3(-15.5f, 85f, -16f);
+        angles[4] = new Vector3(-3.5f, 45.85f, -2.75f);
 
         FOV[0] = 60f;
         FOV[1] = 15f;
         FOV[2] = 10f;
         FOV[3] = 22f;
+        FOV[4] = 55f;
     }
 
     private void Update()
